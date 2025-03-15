fb2281d2b0db07d7c198e5b628892ada618293a1
Kenji MacMartin <kenji_macmartin@sfu.ca>
2025-03-14 23:50:13 -0700

    Autocomplete search boxes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 
 async function initMap() {
     // The location of Vancouver
-    const position = { lat: 49.2827, lng: -123.1207 };
+    const defaultPosition = { lat: 49.2827, lng: -123.1207 };
     // Request needed libraries.
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
@@ -12,25 +12,66 @@ async function initMap() {
     // The map, centered at Vancouver
     map = new Map(document.getElementById("map"), {
         zoom: 9,
-        center: position,
-        mapId: "DEMO_MAP_ID",
+        center: defaultPosition,
+        mapId: "TRIP_PLANNER_ID",
     });
 
+    // Search box element
+    const startInput = document.getElementById("startingLocation");
+    const endInput = document.getElementById("destination");
+
+    // Create autocomplete search boxes
+    const startAutocomplete = new google.maps.places.Autocomplete(startInput);
+    const endAutocomplete = new google.maps.places.Autocomplete(endInput);
+
+    // Create start and end markers
+    const startMarker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+    const endMarker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+    });
+
+    // Place start and end markers
+    function placeMarker(autocomplete, marker) {
+        autocomplete.addListener("place_changed", () => {
+            const place = autocomplete.getPlace();
+            if (!place.geometry) {
+                alert("No details available for this location");
+                return;
+            }
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+            map.setCenter(place.geometry.location);
+            map.setZoom(15);
+            fitBounds();
+        })
+    }
+
+    // Event listeners
+    placeMarker(startAutocomplete, startMarker);
+    placeMarker(endAutocomplete, endMarker);
+
+    /*
     // Add marker on click
     google.maps.event.addListener(map, "click", (event) => {
         // add Marker
         addMarker({location:event.latLng});
-    })
+    });
+    */
 
     /*
     // Add a marker in Vancouver
     const marker = new AdvancedMarkerElement({
         map: map,
-        position: position,
+        position: defaultPosition,
         title: "Vancouver",
     });
     */
 
+    /*
     // Add Marker
     function addMarker(property) {
         const marker = new AdvancedMarkerElement({
@@ -38,8 +79,9 @@ async function initMap() {
             map: map
         });
     }
+    */
 
-    addMarker({location:{ lat: 49.2827, lng: -123.1207 }});
+    //addMarker({location:{ lat: 49.2827, lng: -123.1207 }});
 }
 
 initMap();
