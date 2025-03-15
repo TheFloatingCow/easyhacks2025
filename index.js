5e0e6e1dd00cce3fe4d7534445081e0f518e661a
Kenji MacMartin <kenji_macmartin@sfu.ca>
2025-03-14 23:16:20 -0700

    Update map location

--- index.js
+++ index.js
@@ -2,26 +2,44 @@
 let map;
 
 async function initMap() {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
+    // The location of Vancouver
+    const position = { lat: 49.2827, lng: -123.1207 };
     // Request needed libraries.
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-    // The map, centered at Uluru
+    // The map, centered at Vancouver
     map = new Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 9,
         center: position,
         mapId: "DEMO_MAP_ID",
     });
 
-    // The marker, positioned at Uluru
+    // Add marker on click
+    google.maps.event.addListener(map, "click", (event) => {
+        // add Marker
+        addMarker({location:event.latLng});
+    })
+
+    /*
+    // Add a marker in Vancouver
     const marker = new AdvancedMarkerElement({
         map: map,
         position: position,
-        title: "Uluru",
+        title: "Vancouver",
     });
+    */
+
+    // Add Marker
+    function addMarker(property) {
+        const marker = new AdvancedMarkerElement({
+            position: property.location,
+            map: map
+        });
+    }
+
+    addMarker({location:{ lat: 49.2827, lng: -123.1207 }});
 }
 
 initMap();
